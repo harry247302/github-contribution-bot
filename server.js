53bcb360819743d6a2d80baadc36fab8bb4d4e98
harry247302 <shadab.shaikh.tech@gmail.com>
2026-05-26 16:35:46 +0530

    change

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const jsonfile = require("jsonfile");
 const moment = require("moment");
 const simpleGit = require("simple-git");
+
 const path = "./data.json";
-const date = moment().subtract(5, 'd').format();
+
+const date = moment().subtract(5, "d").format();
+
 const data = {
-  date:date,
-}
-jsonfile.writeFile(path, data, ()=>{
-  simpleGit().add([path]).commit(date,{'--date':date}).push();
+  date: date,
+};
+
+jsonfile.writeFile(path, data, () => {
+  simpleGit()
+    .add([path])
+    .commit(date, { "--date": date })
+    .push();
 });
